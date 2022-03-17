98b2705111f4eae0f9b89ed2593054555b499987
Luis Martinez <leduardo.mp@gmail.com>
2022-03-17 17:31:28 -0600

    Agregando acciones con JavaScript

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 let pokemonID = 0;
+let indexImg = 4;
+const valImg = ['back_default','back_female', 'back_shiny', 'back_shiny_female', 'front_default', 'front_female', 'front_shiny', 'front_shiny_female'];
 
 const abrirCampoBusqueda = () => {
     let element = document.getElementById("modalInput");
@@ -16,13 +18,80 @@ const cancelarCampoBusqueda = () => {
 
 
 const buscarPokemon = () => {
-    
+    const element = document.getElementById("inputNamePokemon");
+    let namePokemon = element.value;
+
+    if (namePokemon !== null && namePokemon !== "") {
+        const url = `https://pokeapi.co/api/v2/pokemon/${namePokemon}`
+        fetch(url).then((res) => { return res.json() })
+            .then((data) => {
+                console.log(data)
+
+                pokemonID = data.id;
+                indexImg = 4;
+
+                const imgPokemon = document.getElementById("imgPokemon");
+                imgPokemon.src = data.sprites[valImg[indexImg]];
+
+                const namePokemon = document.getElementById("namePokemon");
+                namePokemon.innerHTML = data.name;
+
+                const tipoPokemon = document.getElementById("tipoPokemon");
+                tipoPokemon.innerHTML = data.types[0].type.name;
+
+                const pesoPokemon = document.getElementById("pesoPokemon");
+                pesoPokemon.innerHTML = data.weight
+
+                const alturaPokemon = document.getElementById("alturaPokemon");
+                alturaPokemon.innerHTML = data.height
+
+                cancelarCampoBusqueda()
+            })
+    } else {
+
+    }
 }
 
 const nextPokemon = () => {
+    
+    if (pokemonID != 0) {
+        const url = `https://pokeapi.co/api/v2/pokemon/${pokemonID}`
+        fetch(url).then((res) => { return res.json() })
+            .then((data) => {
+                console.log(data)
+                indexImg++;
+                
+                if(indexImg > valImg.length -1){
+                    indexImg = 0;
+                }
 
+                const imgPokemon = document.getElementById("imgPokemon");
+                imgPokemon.src = data.sprites[valImg[indexImg]];
+
+            })
+    } else {
+
+    }
 }
 
 const afterPokemon = () => {
-    
+
+    if (pokemonID != 0) {
+        const url = `https://pokeapi.co/api/v2/pokemon/${pokemonID}`
+        fetch(url).then((res) => { return res.json() })
+            .then((data) => {
+                console.log(data)
+                indexImg--;
+                
+                if(indexImg < 0){
+                    indexImg = valImg.length-1;
+                }
+
+                const imgPokemon = document.getElementById("imgPokemon");
+                imgPokemon.src = data.sprites[valImg[indexImg]];
+
+            })
+    } else {
+
+    }
 }
